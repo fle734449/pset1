4d99f31dceeae4f9a23e6aed2eb9a356446087dc
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-02-18 00:00:06 -0600

    update

--- src/pset1/SLListAddTester.java
+++ src/pset1/SLListAddTester.java
@@ -18,20 +18,20 @@ public class SLListAddTester {
 		
 	}
 	
-@Test public void test1() {
-	SLList l = new SLList();
-	assertTrue(l.repOk());
-	l.add(true);
-	assertTrue(l.repOk());
-	l.add(false);
-	assertTrue(l.repOk());
+	@Test public void test1() {
+		SLList l = new SLList();
+		assertTrue(l.repOk());
+		l.add(true);
+		assertTrue(l.repOk());
+		l.add(false);
+		assertTrue(l.repOk());
 	
-	// write a sequence of assertTrue method invocations that
-	// perform checks on the values for all the declared fields
-	// of list and node objects reachable from l
+		// write a sequence of assertTrue method invocations that
+		// perform checks on the values for all the declared fields
+		// of list and node objects reachable from l
 	
-	assertTrue(l.header != null);
-	// your code goes here
+		assertTrue(l.header != null);
+		// your code goes here
 	
 	}
 }
