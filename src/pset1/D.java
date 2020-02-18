a8bf7f839f91fac488b3e05bfd4f0f17a6174e54
Frank Le <frank.le@utexas.edu>
2020-02-17 23:55:36 -0600

    Added starter code

---
+++ src/pset1/D.java
@@ -0,0 +1,22 @@
+package pset1;
+
+public class D extends C {
+	int g;
+	
+	public D(int f, int g) {
+		super(f);
+		this.g = g;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		// assume this method is implemented for you
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		// assume this method is implemented for you
+		return 0;
+	}
+}
