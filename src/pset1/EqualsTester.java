09c37d110d750f5623fe684979e15deae52a9077
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-02-22 12:46:43 -0600

    finished equalstester

--- src/pset1/EqualsTester.java
+++ src/pset1/EqualsTester.java
@@ -12,22 +12,124 @@ public class EqualsTester {
 		assertFalse(new Object().equals(null));
 	}
 	
-	// your test methods for P1 go here
+	@Test public void t1() {
+		assertFalse(new C(5).equals(null));
+	}
+	
+	@Test public void t2() {
+		assertFalse(new D(5,6).equals(null));
+	}
 	
 	/*
 	 * P2: It is reflexive: for any non-null reference value x, x.equals(x)
 	 * should return true.
 	 */
 	
-	// your test methods for P2 go here
+	@Test public void r0() {
+		Object o = new Object();
+		assertTrue(o.equals(o));
+	}
+	
+	@Test public void r1() {
+		C c = new C(5);
+		assertTrue(c.equals(c));
+	}
+	
+	@Test public void r2() {
+		D d = new D(5,6);
+		assertTrue(d.equals(d));
+	}
 	
 	/*
 	 * P3: It is symmetric: for any non-null reference values x and y, x.equals(y)
 	 * should return true if and only if y.equals(x) returns true.
 	 */
 	
-	// your test methods for P3 go here
+	//Object v Object
+	@Test public void s0() {
+		Object x = new Object();
+		Object y = new Object();
+		assertTrue(x.equals(y) == y.equals(x));
+	}
 	
+	//Object v C
+	@Test public void s1() {
+		Object x = new Object();
+		C y = new C(5);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//Object v D
+	@Test public void s2() {
+		Object x = new Object();
+		D y = new D(5,6);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//C v C
+	@Test public void s3() {
+		C x = new C(5);
+		C y = new C(5);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//C v Object
+	@Test public void s4() {
+		C x = new C(5);
+		Object y = new Object();
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//C v D
+	@Test public void s5() {
+		C x = new C(5);
+		D y = new D(5,6);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//D v D
+	@Test public void s6() {
+		D x = new D(5,6);
+		D y = new D(5,6);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//D v Object
+	@Test public void s7() {
+		D x = new D(5,6);
+		Object y = new Object();
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//D v C
+	@Test public void s8() {
+		D x = new D(5,6);
+		C y = new C(5);
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//Object
+	@Test public void s9() {
+		Object x = new Object();
+		Object y = x;
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//C
+	@Test public void s10() {
+		C x = new C(5);
+		C y = x;
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+	//D
+	@Test public void s11() {
+		D x = new D(5,6);
+		D y = x;
+		assertTrue(x.equals(y) == y.equals(x));
+	}
+	
+
 	/*
 	 * P4: It is transitive: for any non-null reference values x, y, and z,
 	 * if x.equals(y) returns true and y.equals(z) returns true, then
